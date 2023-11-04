3ee12e9ccf556737ddca3e007c5a710471668106
kiyosec <144684055+kiyosec@users.noreply.github.com>
2023-11-04 09:57:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-// JavaScript code moved to an external script.js file
-
 const scanButton = document.getElementById('start-scan');
 const scanResultText = document.getElementById('scan-result-text');
 const downloadReportLink = document.getElementById('download-report');
 
-// Function to simulate the scan
+// Function to simulate the scan with an animated progress bar
 function performScan() {
     scanResultText.innerHTML = 'Scanning...';
 
-    setTimeout(() => {
-        const isInfected = Math.random() < 0.5; // Simulate result (50% chance of infection).
+    const progressBar = document.createElement('div');
+    progressBar.className = 'progress-bar';
+    scanResultText.appendChild(progressBar);
 
-        if (isInfected) {
-            scanResultText.innerHTML = 'Virus Found: The file is infected.';
-        } else {
-            scanResultText.innerHTML = 'No Virus Found: The file is safe.';
+    let progress = 0;
+    const scanInterval = setInterval(() => {
+        progress += 1;
+        progressBar.style.width = `${progress}%`;
+
+        if (progress === 100) {
+            clearInterval(scanInterval);
+            const isInfected = Math.random() < 0.5; // Simulate result (50% chance of infection).
+
+            if (isInfected) {
+                scanResultText.innerHTML = 'Virus Found: The file is infected.';
+            } else {
+                scanResultText.innerHTML = 'No Virus Found: The file is safe.';
+            }
+
+            downloadReportLink.style.display = 'block';
         }
-
-        downloadReportLink.style.display = 'block';
-    }, 2000); // Simulated scanning time (2 seconds).
+    }, 20); // Simulated scanning time (2 seconds).
 }
 
 // Add event listener to the scan button
